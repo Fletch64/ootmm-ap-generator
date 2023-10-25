d4a93cd5dc7bd83bdd8400e85f1b748834996e7e
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2023-10-25 17:33:39 -0500

    Clean up.

--- Program.cs
+++ Program.cs
@@ -1,200 +1,196 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
-using CsvHelper;
-using System.Globalization;
+﻿using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace combineCsvYaml
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace OoTMM.Generators;
+
+public class Record
 {
-    public class Location
+    [Ignore]
+    public string Key { get; set; }
+    [Ignore]
+    public string Game { get; set; }
+    [Name("location")]
+    public string Location { get; set; }
+    [Name("type")]
+    public string Type { get; set; }
+    [Name("hint")]
+    public string Hint { get; set; }
+    [Name("scene")]
+    public string Scene { get; set; }
+    [Name("id")]
+    public string Id { get; set; }
+    [Name("item")]
+    public string Item { get; set; }
+    [Optional]
+    public string Time { get; set; }
+    [Optional]
+    public string Region { get; set; }
+    [Optional]
+    public string Dungeon { get; set; }
+    [Optional]
+    public string Logic { get; set; }
+    [Optional]
+    public string AreaSetName { get; set; }
+    [Optional]
+    public bool AgeChange { get; set; }
+}
+
+public class AreaSet
+{
+    [YamlMember(Alias = "region")]
+    public string Region { get; set; }
+    [YamlMember(Alias = "dungeon")]
+    public string Dungeon { get; set; }
+    [YamlMember(Alias = "time")]
+    public string Time { get; set; }
+    [YamlMember(Alias = "locations")]
+    public Dictionary<string, string> Locations { get; set; }
+    [YamlMember(Alias = "events")]
+    public Dictionary<string, string> Events { get; set; }
+    [YamlMember(Alias = "exits")]
+    public Dictionary<string, string> Exits { get; set; }
+    [YamlMember(Alias = "stay")]
+    public Dictionary<string, string> Stay { get; set; }
+    [YamlMember(Alias = "gossip")]
+    public Dictionary<string, string> GossipStones { get; set; }
+    [YamlMember(Alias = "boss")]
+    public bool Boss { get; set; }
+    [YamlMember(Alias = "age_change")]
+    public bool AgeChange { get; set; }
+}
+
+class Program
+{
+    private static async Task<string> DownloadFileAsync(Uri url)
     {
-        [Ignore]
-        public string key { get; set; }
-        [Ignore]
-        public string game { get; set; }
-        public string location { get; set; }
-        public string type { get; set; }
-        public string hint { get; set; }
-        public string scene { get; set; }
-        public string id { get; set; }
-        public string item { get; set; }
-        [Optional]
-        public string time { get; set; }
-        [Optional]
-        public string region { get; set; }
-        [Optional]
-        public string dungeon { get; set; }
-        [Optional]
-        public string logic { get; set; }
-        [Optional]
-        public string areaSetName { get; set; }
-        [Optional]
-        public bool ageChange { get; set; }
+        using var client = new HttpClient();
+        using var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
     }
 
-    public class AreaSet
+    private static async Task DownloadCsvFileAsync(Uri url, string game, Dictionary<string, Record> data, List<string> keys)
     {
-        public string region;
-        public string dungeon;
-        public string time;
-
-        public Dictionary<string, string> locations;
-        public Dictionary<string, string> events;
-        public Dictionary<string, string> exits;
-        public Dictionary<string, string> stay;
-        public Dictionary<string, string> gossip;
-
-        public bool boss;
-        public bool age_change;
-    }
-
-    class Program
-    {
-        private static string getUrlFile(string url)
+        using var client = new HttpClient();
+        using var response = await client.GetAsync(url);
+        using var stream = await response.Content.ReadAsStreamAsync();
+        using var reader = new StreamReader(stream);
+        using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
         {
-            var webRequest = WebRequest.Create(url);
-            using (var response = webRequest.GetResponse())
-            using (var content = response.GetResponseStream())
-            using (var reader = new StreamReader(content))
+            TrimOptions = TrimOptions.Trim,
+        });
+
+        var records = csv.GetRecords<Record>();
+        foreach (var record in records)
+        {
+            record.Key = game + "-" + record.Location;
+            record.Game = game;
+            data.Add(record.Key, record);
+            keys.Add(record.Key);
+        }
+    }
+
+    static async Task Main()
+    {
+        var urlBase = "https://raw.githubusercontent.com/OoTMM/OoTMM/7293aeb33fa5868fbaea1199d99920e80b0c4459";
+
+        var csvFiles = new[]
+        {
+            ("OOT", "/packages/core/data/oot/pool.csv"),
+            ("MM", "/packages/core/data/mm/pool.csv"),
+        };
+
+        var yamlFiles = new[]
+        {
+            ("OOT","/packages/core/data/oot/world/boss.yml"),
+            ("OOT","/packages/core/data/oot/world/bottom_of_the_well.yml"),
+            ("OOT","/packages/core/data/oot/world/deku_tree.yml"),
+            ("OOT","/packages/core/data/oot/world/dodongo_cavern.yml"),
+            ("OOT","/packages/core/data/oot/world/fire_temple.yml"),
+            ("OOT","/packages/core/data/oot/world/forest_temple.yml"),
+            ("OOT","/packages/core/data/oot/world/ganon_castle.yml"),
+            ("OOT","/packages/core/data/oot/world/ganon_tower.yml"),
+            ("OOT","/packages/core/data/oot/world/gerudo_fortress.yml"),
+            ("OOT","/packages/core/data/oot/world/gerudo_training_grounds.yml"),
+            ("OOT","/packages/core/data/oot/world/ice_cavern.yml"),
+            ("OOT","/packages/core/data/oot/world/jabu_jabu.yml"),
+            ("OOT","/packages/core/data/oot/world/overworld.yml"),
+            ("OOT","/packages/core/data/oot/world/shadow_temple.yml"),
+            ("OOT","/packages/core/data/oot/world/spirit_temple.yml"),
+            ("OOT","/packages/core/data/oot/world/treasure_chest_game.yml"),
+            ("OOT","/packages/core/data/oot/world/water_temple.yml"),
+
+            ("MM", "/packages/core/data/mm/world/ancient_castle_of_ikana.yml"),
+            ("MM", "/packages/core/data/mm/world/beneath_the_well.yml"),
+            ("MM", "/packages/core/data/mm/world/great_bay_temple.yml"),
+            ("MM", "/packages/core/data/mm/world/moon.yml"),
+            ("MM", "/packages/core/data/mm/world/ocean_spider_house.yml"),
+            ("MM", "/packages/core/data/mm/world/overworld.yml"),
+            ("MM", "/packages/core/data/mm/world/pirate_fortress.yml"),
+            ("MM", "/packages/core/data/mm/world/secret_shrine.yml"),
+            ("MM", "/packages/core/data/mm/world/snowhead_temple.yml"),
+            ("MM", "/packages/core/data/mm/world/stone_tower_temple.yml"),
+            ("MM", "/packages/core/data/mm/world/stone_tower_temple_inverted.yml"),
+            ("MM", "/packages/core/data/mm/world/swamp_spider_house.yml"),
+            ("MM", "/packages/core/data/mm/world/woodfall_temple.yml"),
+        };
+
+        var csvData = new Dictionary<string, Record>();
+        var keys = new List<string>();
+        foreach (var (game, file) in csvFiles)
+        {
+            await DownloadCsvFileAsync(new Uri(urlBase + file), game, csvData, keys);
+        }
+
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .Build();
+        foreach (var (game, file) in yamlFiles)
+        {
+            var yamlData = await DownloadFileAsync(new Uri(urlBase + file));
+            var areaSets = deserializer.Deserialize<Dictionary<string, AreaSet>>(yamlData);
+            foreach (var areaSet in areaSets)
             {
-                var strContent = reader.ReadToEnd();
-                return strContent;
+                var locations = areaSet.Value.Locations;
+                if (locations is null) { continue; }
+
+                foreach (var location in locations)
+                {
+                    var record = csvData[game + "-" + location.Key];
+                    record.AreaSetName = areaSet.Key;
+                    record.Region = areaSet.Value.Region;
+                    record.Dungeon = areaSet.Value.Dungeon;
+                    record.Logic = location.Value;
+                }
             }
         }
 
-        private static void getCSVFile(string url, string game, Dictionary<string, Location> dictionary)
+        foreach (var key in keys)
         {
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                //PrepareHeaderForMatch = args => args.Header.Trim(),
-                TrimOptions = TrimOptions.Trim,
-            };
-            var webRequest = WebRequest.Create(url);
-            using (var response = webRequest.GetResponse())
-            using (var content = response.GetResponseStream())
-            using (var reader = new StreamReader(content))
-            using (var csv = new CsvReader(reader, config))
-            {
-                var records = csv.GetRecords<Location>();
-                foreach (var record in records)
-                {
-                    record.key = game + "-" + record.location;
-                    record.game = game;
-                    dictionary.Add(record.key, record);
-                }
-            }
-
-        }
-
-        static void Main(string[] args)
-        {
-            var csvData = new Dictionary<string, Location>();
-
-            getCSVFile(
-                "https://raw.githubusercontent.com/OoTMM/OoTMM/master/packages/core/data/oot/pool.csv",
-                "OOT",
-            csvData);
-            getCSVFile(
-                "https://raw.githubusercontent.com/OoTMM/OoTMM/master/packages/core/data/mm/pool.csv",
-                "MM",
-                csvData);
-
-            var yamlFiles = new[]
-            {
-                ("MM", "/packages/core/data/mm/world/ancient_castle_of_ikana.yml"),
-                ("MM", "/packages/core/data/mm/world/beneath_the_well.yml"),
-                ("MM", "/packages/core/data/mm/world/great_bay_temple.yml"),
-                ("MM", "/packages/core/data/mm/world/moon.yml"),
-                ("MM", "/packages/core/data/mm/world/ocean_spider_house.yml"),
-                ("MM", "/packages/core/data/mm/world/overworld.yml"),
-                ("MM", "/packages/core/data/mm/world/pirate_fortress.yml"),
-                ("MM", "/packages/core/data/mm/world/secret_shrine.yml"),
-                ("MM", "/packages/core/data/mm/world/snowhead_temple.yml"),
-                ("MM", "/packages/core/data/mm/world/stone_tower_temple.yml"),
-                ("MM", "/packages/core/data/mm/world/stone_tower_temple_inverted.yml"),
-                ("MM", "/packages/core/data/mm/world/swamp_spider_house.yml"),
-                ("MM", "/packages/core/data/mm/world/woodfall_temple.yml"),
-                ("OOT","/packages/core/data/oot/world/boss.yml"),
-                ("OOT","/packages/core/data/oot/world/bottom_of_the_well.yml"),
-                ("OOT","/packages/core/data/oot/world/deku_tree.yml"),
-                ("OOT","/packages/core/data/oot/world/dodongo_cavern.yml"),
-                ("OOT","/packages/core/data/oot/world/fire_temple.yml"),
-                ("OOT","/packages/core/data/oot/world/forest_temple.yml"),
-                ("OOT","/packages/core/data/oot/world/ganon_castle.yml"),
-                ("OOT","/packages/core/data/oot/world/ganon_tower.yml"),
-                ("OOT","/packages/core/data/oot/world/gerudo_fortress.yml"),
-                ("OOT","/packages/core/data/oot/world/gerudo_training_grounds.yml"),
-                ("OOT","/packages/core/data/oot/world/ice_cavern.yml"),
-                ("OOT","/packages/core/data/oot/world/jabu_jabu.yml"),
-                ("OOT","/packages/core/data/oot/world/overworld.yml"),
-                ("OOT","/packages/core/data/oot/world/shadow_temple.yml"),
-                ("OOT","/packages/core/data/oot/world/spirit_temple.yml"),
-                ("OOT","/packages/core/data/oot/world/treasure_chest_game.yml"),
-                ("OOT","/packages/core/data/oot/world/water_temple.yml"),
-            };
-            var deserializer = new DeserializerBuilder()
-                .WithNamingConvention(UnderscoredNamingConvention.Instance)
-                .Build();
-            
-            foreach (var (game, file) in yamlFiles)
-            {
-                var yamlData = getUrlFile("https://raw.githubusercontent.com/OoTMM/OoTMM/7293aeb33fa5868fbaea1199d99920e80b0c4459" + file);
-                var areaSets = deserializer.Deserialize<Dictionary<string, AreaSet>>(yamlData);
-                var idCounter = 0;
-                foreach (var areaSet in areaSets)
-                {
-                    var locations = areaSet.Value.locations;
-                    if (locations is null) { continue; }
-
-                    foreach (var location in locations)
-                    {
-                        var record = csvData[game + "-" + location.Key];
-                        record.areaSetName = areaSet.Key;
-                        record.region = areaSet.Value.region;
-                        record.dungeon = areaSet.Value.dungeon;
-                        record.logic = location.Value;
-                        Debug.WriteLine("============");
-                        Debug.WriteLine($"game:\t\t{record.game}");
-                        Debug.WriteLine($"location:\t{record.location}");
-                        Debug.WriteLine($"type:\t\t{record.type}");
-                        Debug.WriteLine($"hint:\t\t{record.hint}");
-                        Debug.WriteLine($"areaSetName:{record.areaSetName}");
-                        Debug.WriteLine($"dungeon:\t{record.dungeon}");
-                        Debug.WriteLine($"scene:\t\t{record.scene}");
-                        Debug.WriteLine($"region:\t\t{record.region}");
-                        Debug.WriteLine($"id:\t\t\t{record.id}");
-                        Debug.WriteLine($"item:\t\t{record.item}");
-                        Debug.WriteLine($"logic:\t\t{record.logic}");
-                        Debug.WriteLine("============");
-
-                    }
-                }
-            }
-            
-            //foreach (var Region in Regions)
-            //{
-            //    var regionName = Region.Key;
-            //    var regionData = (Dictionary<object, object>)Region.Value;
-            //    if (regionData.TryGetValue("locations", out var tempLocations))
-            //    {
-            //        var locations = (Dictionary<object, object>)tempLocations;
-            //        foreach (var location in locations)
-            //        {
-
-            //        }
-            //    }
-            //}
-
+            var record = csvData[key];
+            Debug.WriteLine($"Game:        {record.Game}");
+            Debug.WriteLine($"Location:    {record.Location}");
+            Debug.WriteLine($"Type:        {record.Type}");
+            Debug.WriteLine($"Hint:        {record.Hint}");
+            Debug.WriteLine($"AreaSetName: {record.AreaSetName}");
+            Debug.WriteLine($"Dungeon:     {record.Dungeon}");
+            Debug.WriteLine($"Scene:       {record.Scene}");
+            Debug.WriteLine($"Region:      {record.Region}");
+            Debug.WriteLine($"Id:          {record.Id}");
+            Debug.WriteLine($"Item:        {record.Item}");
+            Debug.WriteLine($"Logic:       {record.Logic}");
+            Debug.WriteLine("============");
         }
     }
 }
-
-// by fletch64 & iryoku
-// https://raw.githubusercontent.com/OoTMM/OoTMM/master/packages/core/data/mm/world/overworld.yml
